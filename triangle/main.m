be78c64ac2f493dafcd327e41ad7d1ba116bc9d5
elskow <xlirby@gmail.com>
2024-12-15 02:38:24 +0700

    Initial Commit

---
+++ triangle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  triangle
+//
+//  Created by Helmy LuqmanulHakim on 15/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
